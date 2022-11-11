20f1855501fe924739782aef4fcabde987316e06
Jenn <jenniferjuarezjj@gmail.com>
2022-11-10 22:31:07 -0500

    add redirecting after user adds car

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Car(models.Model):
@@ -8,8 +8,11 @@ class Car(models.Model):
   make = models.CharField(max_length=100)
   model = models.CharField(max_length=250)
   color = models.CharField(max_length=250)
-  description = models.TextField(max_length=250)
+  # description = models.TextField(max_length=250)
   
   
   def __str__(self):
     return self.owner
+  
+  def get_absolute_url(self):
+    return reverse('cars_detail', kwargs={'car_id': self.id})
