04da7694b017f21ef4d6b9d8158111978bf1c7d0
Jenn <jenniferjuarezjj@gmail.com>
2022-11-10 20:04:23 -0500

    create car models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+  owner = models.CharField(max_length=250)
+  year = models.IntegerField()
+  make = models.CharField(max_length=100)
+  model = models.TextField(max_length=250)
+  color = models.CharField(max_length=250)
+  
+  
+  def __str__(self):
+    return self.owner
--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,18 @@
 from django.shortcuts import render
-
+from .models import Car
 # Add the following import
-from django.http import HttpResponse
 
 
-# Add the Cat class & list and view function below the imports
-class Car:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, year, make, model, color):
-    self.year = year
-    self.make = make
-    self.model = model
-    self.color = color
 
-cars = [
-  Car(2020, 'Honda', 'Civic', 'Black'),
-  Car(1987, 'Ford', 'Mustang', 'Red'),
-  Car(2023, 'Porshe', '911', 'Black'),
-  Car(2023, 'Ferrari', 'Purosangue', 'Grey')
-]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def cars_index(request):
+  cars = Car.objects.all()
   return render(request, 'cars/index.html', { 'cars': cars })
 
