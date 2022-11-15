97552853ab0b9b96212e888679bf3a71c763d4b6
Jenn <jenniferjuarezjj@gmail.com>
2022-11-15 14:56:15 -0500

    successfully modify home.html

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -34,6 +35,7 @@ class Car(models.Model):
   color = models.CharField(max_length=250)
   description = models.TextField(max_length=250)
   rentals = models.ManyToManyField(Rental)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
     return self.owner
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),  
+  path('', views.Home.as_view(), name='home'),  
   path('about/', views.about, name='about'),  
   path('cars/', views.cars_index, name='cars_index'),
   path('cars/<int:car_id>/', views.cars_detail, name='cars_detail'),
